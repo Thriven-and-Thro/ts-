e18cba0afbe608b236dd612edf7dd77d41fcd9d3
cf <475392583@qq.com>
2022-05-12 23:59:17 +0800

    2022.5.12 23.59

--- 通关秘籍/11.ts
+++ 通关秘籍/11.ts
@@ -4,4 +4,23 @@ type ParseParam<Param extends string> =
         [K in Key]: Val;
       }
     : {};
-type ParseQueryString<Str extends string> = Str extends `${infer Param}&${infer Rest}` ? Merge
+
+type MergeValue<OneValue, OtherValue> = OneValue extends OtherValue
+  ? OneValue
+  : OtherValue extends unknown[]
+  ? [OneValue, ...OtherValue]
+  : [OneValue, OtherValue];
+
+type MergeParams<
+  OneParam extends Record<string, any>,
+  OtherParam extends Record<string, any>
+> = {
+  [Key in keyof OneParam | keyof OtherParam]: Key extends keyof OneParam
+    ? Key extends keyof OtherParam
+      ? MergeValue<OneParam[Key], OtherParam[Key]>
+      : OneParam[Key]
+    : Key extends keyof OtherParam
+    ? OtherParam[Key]
+    : never;
+};
+// type ParseQueryString<Str extends string> = Str extends `${infer Param}&${infer Rest}` ? MergeParams<ParseParam<Param>> :
