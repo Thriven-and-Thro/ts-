a3cf6976e8c35f5a75f4b6ba67eba885f6feda51
cf <475392583@qq.com>
2022-05-14 16:02:48 +0800

    2022.5.14 16.02

--- 通关秘籍/load_six.ts
+++ 通关秘籍/load_six.ts
@@ -1,9 +1,10 @@
 type IsAny<T> = 1 extends 2 & T ? true : false;
-type IsAnyResult1 = IsAny<any>;
-type IsAnyResult2 = IsAny<unknown>;
+type IsAnyReult1 = IsAny<any>;
+type IsAnyReult2 = IsAny<unknown>;
 
 type IsUnion<A, B = A> = A extends A ? ([B] extends [A] ? false : true) : never;
-type IsUnionResult = IsUnion<"a" | "b">;
+type IsUnionResult1 = IsUnion<1 | 2>;
+type IsUnionResult2 = IsUnion<1>;
 
 type IsNever<T> = [T] extends [never] ? true : false;
 type IsNeverResult = IsNever<never>;
@@ -16,7 +17,8 @@ type IsEqual<A, B> = (<T>() => T extends A ? 1 : 2) extends <T>() => T extends B
   : 2
   ? true
   : false;
-type IsEqualResult = IsEqual<1, 1>;
+type IsEqualResult1 = IsEqual<any, unknown>;
+type IsEqualResult2 = IsEqual<1, 1>;
 
 type IsTuple<T> = T extends readonly [...infer Ele]
   ? IsEqual<Ele["length"], number> extends true
@@ -27,61 +29,37 @@ type IsTupleResult1 = IsTuple<[1, 2]>;
 type IsTupleResult2 = IsTuple<number[]>;
 
 type UnionToIntersection<U> = (
-  U extends U ? (x: U) => unknown : never
-) extends (x: infer R) => unknown
+  U extends U ? (X: U) => unknown : never
+) extends (X: infer R) => unknown
   ? R
   : never;
-type UnionToIntersectionResult = UnionToIntersection<{ 1: "1" } | { 2: "2" }>;
-// type UnionToIntersectionResult = {
-//   1: "1";
-// } & {
-//     2: "2";
-// }
+// 需要对象的联合类型
+type UnionToIntersectionResult = UnionToIntersection<{ 1: "a" } | { 2: "b" }>;
 
 type GetOptional<Obj extends Record<string, any>> = {
   [Key in keyof Obj as {} extends Pick<Obj, Key> ? Key : never]: Obj[Key];
 };
-type obj1 = {
-  name: string;
-  age?: number;
-};
-type GetOptionalResult = GetOptional<obj1>;
-// type GetOptionalResult = {
-//   age?: number;
-// }
+type GetOptionalReult = GetOptional<{ name: string; age?: number }>;
 
 type GetRequired<Obj extends Record<string, any>> = {
   [Key in keyof Obj as {} extends Pick<Obj, Key> ? never : Key]: Obj[Key];
 };
-type GetRequiredResult = GetRequired<obj1>;
+type GetRequiredResult = GetRequired<{ name: string; age?: number }>;
 
 type RemoveIndexSignature<Obj extends Record<string, any>> = {
   [Key in keyof Obj as Key extends `${infer Str}` ? Str : never]: Obj[Key];
 };
-type obj2 = {
+type RemoveIndexSignatureResult = RemoveIndexSignature<{
   [key: string]: any;
   sleep(): void;
-};
-type RemoveIndexSignatureResult = RemoveIndexSignature<obj2>;
-// type RemoveIndexSignatureResult = {
-//   sleep: () => void;
-// }
+}>;
 
-class Dong {
+class Person {
   public name: string;
   protected age: number;
-  private hobbies: string[];
-
-  constructor() {
-    this.name = "dong";
-    this.age = 20;
-    this.hobbies = ["sleep", "eat"];
-  }
+  private hobbies: any[];
 }
 type ClassPublicProps<Obj extends Record<string, any>> = {
   [Key in keyof Obj]: Obj[Key];
 };
-type ClassPublicPropsResult = ClassPublicProps<Dong>;
-// type ClassPublicPropsResult = {
-//   name: string;
-// }
+type ClassPublicPropsResult = ClassPublicProps<Person>;
